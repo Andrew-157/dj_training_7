e60b8ade7a357884e6f903aeaa1a737479a13876
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-07 20:11:46 +0100

    feat: aggregate and order by title  books list on the index page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse, Http404
-
+from django.db.models import Avg, Min, Max
 
 from .models import Book
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'), Min('rating'), Max('rating'))
     return render(request, template_name='book_outlet/index.html',
-                  context={'books': books})
+                  context={'books': books,
+                           'total_number_of_books': num_books,
+                           'average_rating': avg_rating})
 
 
 def book_detail(request: HttpRequest, slug: str) -> HttpResponse:
